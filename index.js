c476b7befcaf6faa1281f5d9908bf4445e607e53
ye.ibragim <army.ibragim@gmail.com>
2023-04-22 21:12:38 +0600

    commit

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ var pool = createPool({
 });
 
 app.use(
-  cors({
-    origin: "http://127.0.0.1:5173",
-  })
+  cors()
+  // {
+  //   origin: "http://127.0.0.1:5173",
+  // }
 );
 app.use(express.json());
 
@@ -32,15 +33,18 @@ app.get("/orders", (req, result) => {
 
 //метод для получения продуктов по категории
 
-app.get("/products/:product_type", (req, result) => {
-  switch (req.params["product_type"]) {
-    case "food":
-      pool.query(
-        "SELECT * FROM product JOIN product_class on product.class_id=product_class.id where product_class.class_name = 'food';",
-        (err, res) => result.json(res)
-      );
-      break;
-  }
+app.get("/products", (req, result) => {
+  const params = req.query;
+  pool.query(
+    `SELECT * FROM product WHERE supplier_id=${params.supplier_id} `,
+    (err, res) => result.json(res)
+  );
+});
+
+app.get("/product", (req, result) => {
+  const params = req.query;
+  const sql = `SELECT product.id,product_class.class_name,product.price,supplier.title,product.supplier_id,supplier.address,supplier.number,supplier.supplier_type,product.name  FROM product_class JOIN product on product.class_id=product_class.id join supplier on product.supplier_id=supplier.id where product.id=${params.id}`;
+  pool.query(sql, (err, res) => result.json({ data: res }));
 });
 
 // метод для создания заказа
@@ -52,6 +56,40 @@ app.post("/create-order", (req, res) => {
     if (err) throw err;
     console.log(result);
     res.send("Item created");
+  });
+});
+
+//метод для создания пользователя
+app.post("/create-customer", (req, res) => {
+  // Extract the name, address, and number fields from the request body
+  const { name, address, number } = req.body;
+
+  // Define the SQL query string and parameters
+  const sql = "INSERT INTO customer (name, address, number) VALUES (?, ?, ?)";
+  const values = [name, address, number];
+
+  // Get a connection from the connection pool
+  pool.getConnection((err, connection) => {
+    if (err) {
+      // Handle error
+      console.error(`Failed to get MySQL connection: ${err}`);
+      res.status(500).send("Internal server error");
+    } else {
+      // Execute the SQL query with the provided parameters
+      connection.query(sql, values, (err, result) => {
+        // Release the connection back to the pool
+        connection.release();
+
+        if (err) {
+          // Handle error
+          console.error(`Failed to insert customer: ${err}`);
+          res.status(500).send("Internal server error");
+        } else {
+          // Return the ID of the newly created row
+          res.status(201).json({ id: result.insertId });
+        }
+      });
+    }
   });
 });
 
